f50eb40630028ea7158c39e3fc21da04d89ed7f5
Alvaro-Olivie <144257341+Alvaro-Olivie@users.noreply.github.com>
2023-11-05 17:13:56 +0000

    initial fetch gathered

--- Project/DashBoard.js
+++ Project/DashBoard.js
@@ -11,7 +11,7 @@ const url = "https://archive-api.open-meteo.com/v1/archive";
 const responses = await fetchWeatherApi(url, params);
 
 // Helper function to form time ranges
-const range = (start: number, stop: number, step: number) =>
+const range = (start, stop, step) =>
 	Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
 
 // Process first location. Add a for-loop for multiple locations or weather models
@@ -24,18 +24,16 @@ const timezoneAbbreviation = response.timezoneAbbreviation();
 const latitude = response.latitude();
 const longitude = response.longitude();
 
-const daily = response.daily()!;
+const daily = response.daily();
 
 // Note: The order of weather variables in the URL query and the indices below need to match!
 const weatherData = {
-
 	daily: {
 		time: range(Number(daily.time()), Number(daily.timeEnd()), daily.interval()).map(
 			(t) => new Date((t + utcOffsetSeconds) * 1000)
 		),
-		temperature2mMean: daily.variables(0)!.valuesArray()!,
+		temperature2mMean: daily.variables(0).valuesArray(),
 	},
-
 };
 
 // `weatherData` now contains a simple structure with arrays for datetime and weather data
@@ -44,4 +42,8 @@ for (let i = 0; i < weatherData.daily.time.length; i++) {
 		weatherData.daily.time[i].toISOString(),
 		weatherData.daily.temperature2mMean[i]
 	);
+}
+
+const getWeatherData = () => {
+	return weatherData;
 }
